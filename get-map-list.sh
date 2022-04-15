7e2f0e244b7995b00540a6772fea7cbc11477895
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2022-04-15 09:57:23 +0200

    fix: update regex
    Now also matches map names that have numbers

--- get-map-list.sh
+++ get-map-list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #./get-map-list.py
-curl -s https://tempus.xyz/api/maps/list | grep -o "\"name\": \"[a-zA-Z_]*\"" | cut -d' ' -f 2 | cut -c 2- | sed 's/.$//' > maps.csv
+curl -s https://tempus.xyz/api/maps/list | grep -o "\"name\": \"[a-zA-Z0-9_]*\"" | cut -d' ' -f 2 | cut -c 2- | sed 's/.$//' > maps.csv
 
 echo maps.csv file created
